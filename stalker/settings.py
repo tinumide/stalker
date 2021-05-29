641e633b45ae61ce67bd9b3e79c0d48f921ef7d2
tinumide <summittinuade@gmail.com>
2021-05-29 02:24:20 +0100

    fix

--- stalker/settings.py
+++ stalker/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^k@6ety=4l37^48e9luomh8%q+mhve%tffa5f^b@sptkqtq98f'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
